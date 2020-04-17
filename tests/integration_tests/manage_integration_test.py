5548efca5eea111171e88de8de862f5e29a154ac
ronykoz <rkozakish@paloaltonetworks.com>
2020-04-17 23:37:17 +0300

    skipping test

--- tests/integration_tests/manage_integration_test.py
+++ tests/integration_tests/manage_integration_test.py
@@ -1,5 +1,6 @@
 import os
 
+import pytest
 from click.testing import CliRunner
 
 from src.cli import main
@@ -9,6 +10,7 @@ from src.common import MANAGE_COMMAND_NAME
 MOCK_FOLDER_PATH = os.path.join(os.getcwd(), "tests", "mock_data")
 
 
+@pytest.mark.skip(reason="Not ready yet")
 def test_manage_positive_scenario(mocker):
     runner = CliRunner()
     result = runner.invoke(main, [MANAGE_COMMAND_NAME, '-c', os.path.join(MOCK_FOLDER_PATH, 'conf.ini')])
